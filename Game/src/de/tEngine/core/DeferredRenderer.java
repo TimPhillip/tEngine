85703f413b026a186b296ede45b012ab3de663cd
Tim <Tim@Tims-PC.fritz.box>
2015-01-09 22:54:13 +0100

    Restructuring of the deferred renderer completed

--- Game/src/de/tEngine/core/DeferredRenderer.java
+++ Game/src/de/tEngine/core/DeferredRenderer.java
@@ -3,8 +3,11 @@ package de.tEngine.core;
 import java.util.List;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
 import de.tEngine.components.PointLight;
@@ -16,9 +19,10 @@ import de.tEngine.shaders.*;
 public class DeferredRenderer {
 
 	private GBuffer gBuffer;
-	private boolean showGBuffer = true;
+	private boolean showGBuffer = false;
 	private DirectionalLightPassShader dirLightShader;
 	private PointLightPassShader pointLightShader;
+	private StencilPassShader stencilShader;
 
 	public void init() {
 		gBuffer = new GBuffer();
@@ -26,10 +30,11 @@ public class DeferredRenderer {
 				.getHeight());
 		dirLightShader = new DirectionalLightPassShader();
 		pointLightShader = new PointLightPassShader();
+		stencilShader = new StencilPassShader();
 	}
 
 	public void render(Scene s) {
-		// gBuffer.startFrame();
+		gBuffer.startFrame();
 		geometryPass(s);
 		if (showGBuffer) {
 			drawGBufferElements();
@@ -61,7 +66,7 @@ public class DeferredRenderer {
 
 	private void drawGBufferElements() {
 		GL30.glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, 0);
-		GL11.glClearColor(0, 1, 0, 1);
+		GL11.glClearColor(0, 0, 0, 1);
 		GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT);
 		gBuffer.bindForReading();
 		int halfWidth = 1280 / 2;
@@ -81,17 +86,31 @@ public class DeferredRenderer {
 	}
 
 	private void lightPass(Scene s) {
+		//Set up light pass
 		GL11.glClearColor(0, 0, 0, 1);
 		GL11.glDisable(GL11.GL_CULL_FACE);
 		GL11.glDisable(GL11.GL_DEPTH_TEST);
-		// Enable blending
+		enableBlending();
+		GL11.glDepthMask(false);
+		gBuffer.bindForLightingPass();
+		
+		pointLightPass(s);
+		directionalLightPass(s);
+		
+		//Clean up light pass
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glCullFace(GL11.GL_BACK);
+		GL11.glDisable(GL11.GL_BLEND);
+	}
+	
+	private void enableBlending(){
 		GL11.glEnable(GL11.GL_BLEND);
 		GL14.glBlendEquation(GL14.GL_FUNC_ADD);
 		GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_ONE);
-		GL11.glDepthMask(false);
-		gBuffer.bindForLightingPass();
-		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-		// directional light
+	}
+
+	private void directionalLightPass(Scene s) {
 		dirLightShader.bind();
 		LightBoundingVolume.screenQuad.bind();
 		dirLightShader.SetUpTextureUnits();
@@ -99,30 +118,57 @@ public class DeferredRenderer {
 		dirLightShader.SetWorldViewProj(Matrix4f.identity());
 		LightBoundingVolume.screenQuad.draw();
 		Shader.unbind();
-		// point lights
-		GL11.glCullFace(GL11.GL_FRONT);
-		GL11.glEnable(GL11.GL_CULL_FACE);
-		LightBoundingVolume.sphere.bind();
-		pointLightShader.bind();
+	}
+
+	private void pointLightPass(Scene s) {
+		GL11.glEnable(GL11.GL_STENCIL_TEST);
+
 		Matrix4f viewProj = Matrix4f.mul(s.getCamera().getViewMatrix(), s
 				.getCamera().getProjectionMatrix());
 		for (PointLight p : s.lights) {
+			//Set up stencil pass
+			stencilShader.bind();
+			gBuffer.bindForStencilPass();
+			GL11.glEnable(GL11.GL_DEPTH_TEST);
+			GL11.glDisable(GL11.GL_CULL_FACE);
+			GL11.glDisable(GL11.GL_BLEND);
+			GL11.glStencilFunc(GL11.GL_ALWAYS, 0, 0);
+			GL20.glStencilOpSeparate(GL11.GL_BACK, GL11.GL_KEEP, GL14.GL_INCR_WRAP, GL11.GL_KEEP);
+			GL20.glStencilOpSeparate(GL11.GL_FRONT, GL11.GL_KEEP, GL14.GL_DECR_WRAP, GL11.GL_KEEP);
+			//Stencil pass
+			GL11.glClear(GL11.GL_STENCIL_BUFFER_BIT);
+			LightBoundingVolume.sphere.bind();
+			Matrix4f wvp = Matrix4f.mul(p.getToWorldMatrix(), viewProj);
+			stencilShader.SetWorldViewProjMatrix(wvp);
+			p.getBoundingVolume().draw();
+			Shader.unbind();
+			//Set up light pass
+			gBuffer.bindForLightingPass();
+			enableBlending();
+			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			GL11.glCullFace(GL11.GL_FRONT);
+			GL11.glEnable(GL11.GL_CULL_FACE);
+			GL11.glStencilFunc(GL11.GL_NOTEQUAL, 0, 0xFF);
+			LightBoundingVolume.sphere.bind();
+			pointLightShader.bind();
+			//light pass
 			pointLightShader.SetUpTextureUnits();
 			pointLightShader.SetPointLight(p);
 			pointLightShader.SetWorldViewProj(Matrix4f.mul(
 					p.getToWorldMatrix(), viewProj));
 			LightBoundingVolume.sphere.draw();
 		}
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glCullFace(GL11.GL_BACK);
-		GL11.glDisable(GL11.GL_BLEND);
+		
+		GL11.glDisable(GL11.GL_STENCIL_TEST);
 	}
 
 	private void finalPass(Scene s) {
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
 		gBuffer.bindForFinalPass();
 		GL30.glBlitFramebuffer(0, 0, Machine.getInstance().getWidth(), Machine
 				.getInstance().getHeight(), 0, 0, Machine.getInstance()
 				.getWidth(), Machine.getInstance().getHeight(),
 				GL11.GL_COLOR_BUFFER_BIT, GL11.GL_LINEAR);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
 	}
 }
--- Game/src/de/tEngine/core/GBuffer.java
+++ Game/src/de/tEngine/core/GBuffer.java
@@ -6,7 +6,9 @@ import java.nio.IntBuffer;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL21;
 import org.lwjgl.opengl.GL30;
 
 public class GBuffer {
@@ -52,16 +54,15 @@ public class GBuffer {
 		
 		//Initialize the depth buffer
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, depthTexture.getId());
-		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL30.GL_DEPTH_COMPONENT32F, windowWidth, windowHeight, 0, GL11.GL_DEPTH_COMPONENT, GL11.GL_FLOAT, (ByteBuffer)null);
-		
-		GL30.glFramebufferTexture2D(GL30.GL_DRAW_FRAMEBUFFER,GL30.GL_DEPTH_ATTACHMENT, GL11.GL_TEXTURE_2D, depthTexture.getId(), 0);
+		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL30.GL_DEPTH32F_STENCIL8, windowWidth, windowHeight, 0, GL30.GL_DEPTH_STENCIL, GL30.GL_FLOAT_32_UNSIGNED_INT_24_8_REV, (ByteBuffer)null);
+		GL30.glFramebufferTexture2D(GL30.GL_DRAW_FRAMEBUFFER,GL30.GL_DEPTH_STENCIL_ATTACHMENT, GL11.GL_TEXTURE_2D, depthTexture.getId(), 0);
 		
 		//Initialize the final texture
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, finalTexture.getId());
 		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, windowWidth,windowHeight, 0, GL11.GL_RGBA, GL11.GL_FLOAT, (ByteBuffer)null);
 		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
 		GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-		GL30.glFramebufferTexture2D(GL11.GL_TEXTURE_2D, GL30.GL_COLOR_ATTACHMENT4, GL11.GL_TEXTURE_2D, finalTexture.getId(), 0);
+		GL30.glFramebufferTexture2D(GL30.GL_DRAW_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT4, GL11.GL_TEXTURE_2D, finalTexture.getId(), 0);
 		
 		
 		drawBuffers = BufferUtils.createIntBuffer(3);
@@ -100,7 +101,6 @@ public class GBuffer {
 	public void bindForLightingPass(){
 		GL30.glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, fboID);
 		GL11.glDrawBuffer(GL30.GL_COLOR_ATTACHMENT4);
-		//GL30.glBindFramebuffer(GL30.GL_DRAW_FRAMEBUFFER, 0);
 		for(int i =0; i < textures.length; i++){
 			GL13.glActiveTexture(GL13.GL_TEXTURE0 + i);
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, textures[i].getId());
